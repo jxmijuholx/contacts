89bc04d1c21ba0f3cd4260f72dc3552232b97b95
jxmijuholx <jami.juhola01@gmail.com>
2024-03-08 02:02:45 +0200

    done

--- contacts/App.js
+++ contacts/App.js
@@ -1,11 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as Contacts from 'expo-contacts';
+import React, { useState } from 'react';
+import { Button, FlatList, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync({
+        fields: [Contacts.Fields.Name, Contacts.Fields.PhoneNumbers],
+      });
+      if (data.length === 0) {
+        return;
+      }
+      const formattedContacts = data.map(contact => {
+        const name = contact.name || '';
+      
+        let phoneNumber = 'N/A';
+        if (contact.phoneNumbers.length > 0) {
+          const firstPhoneNumber = contact.phoneNumbers[0];
+          if (firstPhoneNumber.number) {
+            phoneNumber = firstPhoneNumber.number;
+          }
+        }
+        return { name, phoneNumber };
+      });
+      setContacts(formattedContacts);
+      
+    } else {
+      console.log('Contacts permission denied');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title="Get Contacts" onPress={getContacts} />
+      <FlatList
+        data={contacts}
+        renderItem={({ item }) => (
+          <View style={styles.contactContainer}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text style={styles.phoneNumber}>{item.phoneNumber}</Text>
+          </View>
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </View>
   );
 }
@@ -14,7 +54,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 50,
+    paddingHorizontal: 20,
+  },
+  contactContainer: {
+    marginBottom: 10,
+  },
+  name: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  phoneNumber: {
+    fontSize: 16,
+    color: 'gray',
   },
 });
